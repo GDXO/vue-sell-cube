9e7e760774c66d4dfb4f51eff5ba6dc46bfdd13d
lcy668 <liuchaoyanzi1103@163.com>
2023-12-22 16:24:11 +0800

    chore: 构建项目

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '',
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.devtool = false
+    } else {
+      config.devtool = 'cheap-module-source-map'
+    }
+  }
 })
