300706291793fe1da13b79121f2792b1efdcfa8d
lcy668 <liuchaoyanzi1103@163.com>
2023-12-25 11:23:28 +0800

    chore: 增加了数据 mock; 修改默认行尾序列为 CRLF;

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
+
+// mock 数据
+const appData = require('./data.json')
+const seller = appData.seller
+const goods = appData.goods
+const ratings = appData.ratings
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '',
@@ -7,6 +14,49 @@ module.exports = defineConfig({
       config.devtool = false
     } else {
       config.devtool = 'cheap-module-source-map'
+    }
+  },
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        import: ['./src/theme']
+      }
+    }
+  },
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: true
+    }
+  },
+  devServer: {
+    setupMiddlewares (middlewares, devServer) {
+      // 返回 seller 数据
+      devServer.app.get('/api/seller', (req, res) => {
+        res.json({
+          errNo: 0,
+          data: seller
+        })
+      })
+
+      // 返回 goods 数据
+      devServer.app.get('/api/goods', (req, res) => {
+        res.json({
+          errNo: 0,
+          data: goods
+        })
+      })
+
+      // 返回 ratings 数据
+      devServer.app.get('/api/ratings', (req, res) => {
+        res.json({
+          errNo: 0,
+          data: ratings
+        })
+      })
+
+      return middlewares
     }
   }
 })
